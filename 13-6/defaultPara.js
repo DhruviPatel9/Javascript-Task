dda38c5b314ef4b2489cce91b0719823c168d908
DhruviPatel9 <126544268+DhruviPatel9@users.noreply.github.com>
2024-06-17 21:11:23 +0530

    success

---
+++ 13-6/defaultPara.js
@@ -0,0 +1,20 @@
+function add(x=6,y=4){
+    return x+y;
+}
+console.log(add());
+
+// function declaration
+function add1(num1,num2){
+    return num1+num2;
+}
+
+// calling function
+let result = add1(10, 40);
+console.log(result);
+
+function threenumber(x,y,z){
+    return x+y+z;
+}
+
+let result1 = threenumber(5,5,5);
+console.log(result1);
